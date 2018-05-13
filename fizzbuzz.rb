5526557da037c8f4c1a8c27931ece7b9bb0da68f
Petro Tarnavskyy <petro.tarnavskyy@lnu.edu.ua>
2018-05-13 19:18:17 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,11 @@
-def fizzbuzz(intx)
-  if intx % 3 == 0
-    "Fizz"
-  end
-  if intx % 5 == 0
+def fizzbuzz(int)
+  if int%15==0
+    "FizzBuzz"
+  elsif int%5==0
     "Buzz"
+  elsif int%3==0
+    "Fizz"
+  else
+    return nil
   end
 end
